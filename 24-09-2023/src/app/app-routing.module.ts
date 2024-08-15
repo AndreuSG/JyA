20769ab8a5cff1d785a713940ffd396423fcd3a3
Andreu <andreu.s.g30@gmail.com>
2024-08-15 14:04:55 +0200

    refactor(features to pages): Pages is more intuitive for this app

--- 24-09-2023/src/app/app-routing.module.ts
+++ 24-09-2023/src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+
+// Rutas de la aplicación cargadas con lazy loading
+const routes: Routes = [
+  {
+    path: 'nuestra-historia',
+    loadChildren: () => import('./pages/historia/historia.module').then(m => m.HistoriaModule)
+  },
+  {
+    path: 'viajes',
+    loadChildren: () => import('./pages/viajes/viajes.module').then(m => m.ViajesModule)
+  },
+  {
+    path: 'momentos-especiales',
+    loadChildren: () => import('./pages/momentos-especiales/momentos-especiales.module').then(m => m.MomentosEspecialesModule)
+  },
+  {
+    path: 'futuro',
+    loadChildren: () => import('./pages/futuro/futuro.module').then(m => m.FuturoModule)
+  },
+  {
+    path: 'galeria',
+    loadChildren: () => import('./pages/galeria/galeria.module').then(m => m.GaleriaModule)
+  },
+  {
+    path: 'sorpresa-especial',
+    loadChildren: () => import('./pages/sorpresa-especial/sorpresa-especial.module').then(m => m.SorpresaEspecialModule)
+  },
+  {
+    path: '', redirectTo: '/nuestra-historia', pathMatch: 'full' // Redirección a una ruta predeterminada
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
