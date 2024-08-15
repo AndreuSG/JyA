7da10808c156761b3849af65bb50d48aee3cae4e
Andreu <andreu.s.g30@gmail.com>
2024-08-15 13:49:18 +0200

    feat(primeModule): Add prime module to encapsulate primeng components

---
+++ 24-09-2023/src/app/prime/prime.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BreadcrumbModule
+  ]
+})
+export class PrimeModule { }
